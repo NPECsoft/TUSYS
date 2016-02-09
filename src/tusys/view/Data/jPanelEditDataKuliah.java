b3dbd67fe81b75b75ffd350f5d4a6eaa95d63492
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-02-09 11:36:03 +0700

    data kuliah dan ruang add validation

--- src/tusys/view/Data/jPanelEditDataKuliah.java
+++ src/tusys/view/Data/jPanelEditDataKuliah.java
@@ -147,6 +147,14 @@ public class jPanelEditDataKuliah extends javax.swing.JPanel {
                 JOptionPane.showMessageDialog(null, "Panjang kode kuliah max. 6 karakter");
                 return;
             }
+            try{
+                Integer.parseInt(jTextFieldJumlahPeserta.getText());
+            }catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "angka jumlah peserta salah");
+                return;
+            }
+            
+            //to query
             Kuliah k = new Kuliah(jTextFieldKodeKuliah.getText(),
                     jTextFieldNamaKuliah.getText(),
                     Integer.parseInt(jTextFieldJumlahPeserta.getText()));
